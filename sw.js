99b86e09307d771bc6883ede0e068492ff8facfa
badatt <badatt@users.noreply.github.com>
2024-04-14 04:40:40 +0000

    deploy: b0fdc48ddc67816fcf91a5fc6000f2bb5040ede9

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bba2a38728f37eb3c685.js"
+    "url": "webpack-runtime-4b4ba3499fbbfe8e2266.js"
   },
   {
     "url": "framework-a57f017859b38f871106.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "28b7917c9ebd899c741caa7ca1384da1"
+    "revision": "0615432358c1eb7b103fc3225efaab7b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a68127634f725ba5d352.js"
